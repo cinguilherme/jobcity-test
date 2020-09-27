805423a52f509c07777b9e8af8b9ad9e84cdd55f
Guilherme Cintra <cinguilherme@gmail.com>
2020-09-26 22:34:06 -0300

    setup initial presentation of context path on console

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -12,22 +12,31 @@ import scorecalculator.ScoreCalculator;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
     public static void main(String[] args) {
-        Stream<String> filesPath = Arrays.stream(args).map(String::valueOf);
+        List<String> filesPath = Arrays.stream(args).map(String::valueOf).collect(toList());
 
-        System.out.println("Bowling game interpreter:");
+        System.out.println("#############################################");
+        System.out.println("Bowling Game Interpreter:");
+
+        String workdir = System.getProperty("user.dir");
+        System.out.println("Working Directory = " + workdir);
+        filesPath.stream().forEach(fileName -> {
+            System.out.println("Filename: " + fileName);
+            System.out.println("Windows: Lookup Filename: " + workdir + "\\" + fileName);
+        });
+        System.out.println("#############################################");
 
         MatchParser fileParser = new MatchTextParser();
         MatchBuilder bowlingMatchBuilder = new BowlingMatchBuilder();
         ScoreCalculator scoreCalculator = new BowlingScoreCalculator();
 
         filesPath.forEach(fPath -> {
-            List<Chance> chanceStream = fileParser.parseInput(fPath);
+            List<Chance> chanceStream = fileParser.parseInput(workdir + "\\" + fPath);
             Boolean isValidData = bowlingMatchBuilder.validatePlayerMatchData(chanceStream);
 
             if (isValidData) {
@@ -36,7 +45,7 @@ public class Main {
 
                 List<List<FrameScore>> allPlayersScores = playersMap.values().stream()
                         .map(bowlingMatchBuilder::getPlayersChancesAsFrameScore)
-                        .map(scoreCalculator::calculateFramesScores).collect(Collectors.toList());
+                        .map(scoreCalculator::calculateFramesScores).collect(toList());
 
             }
 
